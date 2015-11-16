1c999fe3727754b654201a5f9ad32aac85df0b57
Yifan Wang <wyf7107@gmail.com>
2015-11-15 21:48:20 -0500

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Yifan Wang on 15/11/15.
+//  Copyright © 2015年 Yifan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Ribbit
+//
+//  Created by Yifan Wang on 15/11/15.
+//  Copyright © 2015年 Yifan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
